74fe7a4949975054b6625598966b2aa0f772b228
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-10-05 16:59:28 +0200

    tests for ordering products add

--- src/test/java/com/codecool/VendingMachineTest.java
+++ src/test/java/com/codecool/VendingMachineTest.java
@@ -2,7 +2,6 @@ package com.codecool;
 
 import com.codecool.model.Coin;
 import com.codecool.model.Product;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -37,5 +36,26 @@ class VendingMachineTest {
         vendingMachine.insertCoin(Coin.DIME);
         vendingMachine.insertCoin(Coin.DIME);
         assertEquals(vendingMachine.getInsertedCoins().get(Coin.DIME),2);
+    }
+
+    @Test
+    void shouldAddNewProductIntoOrderedProducts_whenProductOrdered(){
+        vendingMachine.updateOrderedProducts(Product.COLA);
+        assertEquals(vendingMachine.getOrderedProducts().get(Product.COLA),1);
+    }
+
+    @Test
+    void shouldUpdateProductAmount_whenSecondSameProductOrdered(){
+        vendingMachine.updateOrderedProducts(Product.COLA);
+        vendingMachine.updateOrderedProducts(Product.COLA);
+        assertEquals(vendingMachine.getOrderedProducts().get(Product.COLA),2);
+    }
+
+    @Test
+    void shouldNotAddMoreProductToOrderedProducts_whenProductOutOfStock(){
+        vendingMachine.getStock().addToStock(Product.COLA, 1);
+        vendingMachine.updateOrderedProducts(Product.COLA);
+        vendingMachine.updateOrderedProducts(Product.COLA);
+        assertEquals(vendingMachine.getOrderedProducts().get(Product.COLA),1);
     }
 }
