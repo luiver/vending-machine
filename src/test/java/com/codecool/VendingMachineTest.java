54a71a5571fd9315fee086cd2533713cf391af20
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-10-05 16:16:36 +0200

    fix tests

--- src/test/java/com/codecool/VendingMachineTest.java
+++ src/test/java/com/codecool/VendingMachineTest.java
@@ -3,27 +3,33 @@ package com.codecool;
 import com.codecool.model.Coin;
 import com.codecool.model.Product;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class VendingMachineTest {
-    private static VendingMachine vendingMachine;
+    private VendingMachine vendingMachine;
 
-    @BeforeAll
-    static void init(){
+    @BeforeEach
+    void beforeEach() {
         vendingMachine = new VendingMachine();
     }
 
     @Test
     void shouldReturnFalse_whenInvalidCoinInserted(){
+        assertFalse(vendingMachine.insertCoin(Coin.PENNY));
+    }
+
+    @Test
+    void shouldReturnNull_whenInvalidCoinInserted(){
         vendingMachine.insertCoin(Coin.PENNY);
-        assertEquals(vendingMachine.getStock().getProductsOnStock().get(Product.COLA),1);
+        assertEquals(vendingMachine.getInsertedCoins().get(Coin.DIME),null);
     }
 
     @Test
     void shouldReturnTrue_whenCoinInserted(){
         vendingMachine.insertCoin(Coin.DIME);
-        assertEquals(vendingMachine.getMachineWallet().getCoins().get(Coin.DIME),1);
+        assertEquals(vendingMachine.getInsertedCoins().get(Coin.DIME),1);
     }
 }
